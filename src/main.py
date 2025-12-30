0a8791e4f82ad45effbcb44e1b89029f069c3705
dotpmm <pmmdot@gmail.com>
2025-12-30 08:33:09 +0530

    Fix API response handling in main module

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ RETRY_DELAYS = [60, 300, 900]
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format='%(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
 
